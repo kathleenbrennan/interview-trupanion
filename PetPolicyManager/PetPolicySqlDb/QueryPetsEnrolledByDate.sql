dc26fe020ebd9ebcf65ccc0cef1e10563ef6fe77
kathleenbrennan <kbrennan206@gmail.com>
2017-06-19 15:30:24 -0700

    Add new database queries for pets by breed and by date of birth

--- PetPolicyManager/PetPolicySqlDb/QueryPetsEnrolledByDate.sql
+++ PetPolicyManager/PetPolicySqlDb/QueryPetsEnrolledByDate.sql
@@ -1,26 +1,28 @@
 ﻿-- PROBLEM C: Question 1A
 -- Count how many pets were enrolled on the day Mount St. Helens erupted
 
-DECLARE @searchDate DATETIME
+DECLARE @searchDate DATE
 
 SET @searchDate = '1980-05-18'
 
-SELECT *
+SELECT COUNT(pet.PetId) As PetsCount
 	FROM Pet as pet
 	INNER JOIN PetPolicy as pp 	
 		ON pet.PetId = pp.PetId
 	INNER JOIN Policy as pol
 		ON pp.PolicyId = pol.PolicyId
 	--search date should be on or after the policy enrollment date
-	WHERE @searchDate >= pol.PolicyEnrollmentDate 
+	WHERE pol.PolicyEnrollmentDate IS NOT NULL
+	AND @searchDate >= pol.PolicyEnrollmentDate 
 	--pet should have been added to the policy on or previous to the search date
+	AND pp.AddToPolicyDate IS NOT NULL
 	AND @searchDate >= pp.AddToPolicyDate 
 	--policy should not have been cancelled previous to the search date
 	--ok if policy was cancelled the same day
-	AND @searchDate <= pol.PolicyCancellationDate 
+	AND (pol.PolicyCancellationDate IS NULL OR @searchDate <= pol.PolicyCancellationDate) 
 	--pet should not have been removed from policy previous to the search date
 	--ok if pet was removed the same day
-	AND @searchDate <= pp.RemoveFromPolicyDate 
+	AND (pp.RemoveFromPolicyDate IS NULL OR @searchDate <= pp.RemoveFromPolicyDate)
 
 
 
--- PetPolicyManager/PetPolicySqlDb/dbo.Pet.data.sql
+++ PetPolicyManager/PetPolicySqlDb/dbo.Pet.data.sql
@@ -1,6 +1,8 @@
-﻿SET IDENTITY_INSERT [dbo].[Pet] ON
+SET IDENTITY_INSERT [dbo].[Pet] ON
 INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (2, 1, N'Frank', N'2011-07-13', 1)
 INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (3, 1, N'InvisibleCat', N'1976-01-01', 1)
 INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (4, 1, N'OtherCat', N'1980-01-01', 1)
-INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (5, 1, N'ThirdCat', N'1981-01-01', NULL)
+INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (5, 1, N'ThirdCat', N'1981-01-01', 1)
+INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (6, 1, N'Hugo', N'2006-07-05', 1)
+INSERT INTO [dbo].[Pet] ([PetId], [OwnerId], [PetName], [PetDateOfBirth], [BreedId]) VALUES (7, 1, N'Emma', N'2013-07-01', 3)
 SET IDENTITY_INSERT [dbo].[Pet] OFF

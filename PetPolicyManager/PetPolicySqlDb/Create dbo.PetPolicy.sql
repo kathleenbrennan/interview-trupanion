f90b15281180a8630a4c7ab797cd6d5fc5fb51a8
kathleenbrennan <kbrennan206@gmail.com>
2017-06-19 13:26:58 -0700

    Add join table to track Pet to Policy relationships

---
+++ PetPolicyManager/PetPolicySqlDb/Create dbo.PetPolicy.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[PetPolicy] (
+    [PetPolicyId]          INT      NOT NULL,
+    [PetId]                INT      NOT NULL,
+    [PolicyId]             INT      NOT NULL,
+    [AddToPolicyDate]      DATETIME NOT NULL,
+    [RemoveFromPolicyDate] DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([PetPolicyId] ASC),
+    CONSTRAINT [FK_PetPolicy_ToPet] FOREIGN KEY ([PetId]) REFERENCES [dbo].[Pet] ([PetId]),
+    CONSTRAINT [FK_PetPolicy_ToPolicy] FOREIGN KEY ([PolicyId]) REFERENCES [dbo].[Policy] ([PolicyId])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_PetPolicy_AddToPolicyDate]
+    ON [dbo].[PetPolicy]([AddToPolicyDate] ASC);
+
